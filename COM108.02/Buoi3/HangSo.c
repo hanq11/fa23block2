9bfaa79761c1faa3ce4afa22570b1d27aa4f9131
Michael <>
2023-11-07 17:19:24 +0700

    update buoi 3

---
+++ COM108.02/Buoi3/HangSo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Khai bao hang so CHIEUDAI
+#define CHIEUDAI 30
+
+int main() {
+    // Khai bao hang so CHIEURONG
+    const int CHIEURONG = 40;
+    printf("Chieu dai la: %d\n", CHIEUDAI);
+    printf("Chieu rong la: %d", CHIEURONG);
+    return 0;
+}
---
+++ COM108.02/Buoi3/KhaiBaoBien.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    // Khai bao bien
+    int bienInt = 1;
+    float bienFloat = 1.1;
+    char bienChar = 'a';
+
+    // In bien
+    printf("Bien int la: %d \n", bienInt);
+    printf("Bien float la: %f \n", bienFloat);
+    printf("Bien char la: %c", bienChar);
+    
+    return 0;
+}
