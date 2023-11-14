852f002c4e0f33386558a92cb417bf3845c41c99
Michael <>
2023-11-14 15:09:01 +0700

    update buoi 6

---
+++ COM108.01/Buoi6/SwitchCaseWithoutDefault.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    printf("Viet Nam có bao nhiêu dân tộc anh em: \n");
+    printf("a. 52\nb. 53\nc. 54\nd. 55\n");
+    printf("Mời bạn chon đáp án a, b, c hoặc d: ");
+    char dapAn;
+    scanf("%c", &dapAn);
+    switch(dapAn) {
+        case 'a':
+            printf("Dap an sai");
+            break;
+        case 'b':
+            printf("Dap an sai");
+            break;
+        case 'c':
+            printf("Dap an dung");
+            break;
+        case 'd':
+            printf("Dap an sai");
+            break;
+    }
+ }
