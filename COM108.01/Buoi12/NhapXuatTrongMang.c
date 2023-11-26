c4df323001d4c20a0725858d65b6d75e081bd133
Michael <>
2023-11-26 10:54:51 +0700

    update

---
+++ COM108.01/Buoi12/NhapXuatTrongMang.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    float mangFloat[5];
+    int i;
+    for(i = 0; i < 5; i++) {
+        printf("Vui lòng nhập giá trị cho phần tử %d: ", i);
+        scanf("%f", &mangFloat[i]);
+    }
+    for(i = 0; i < 5; i++) {
+        printf("Giá trị phần tử %d của mảng là: %f \n", i, mangFloat[i]);
+    }
+    float x;
+    printf("Vui lòng nhập số x cần tìm: ");
+    scanf("%f", &x);
+    // Đặt i = 0 vì sau khi chạy 1 vòng for, i = 5
+    i = 0;
+    while(i < 5) {
+        if(x == mangFloat[i]) {
+            printf("X có xuất hiện trong mảng");
+        }
+        i++;
+    }
+}
